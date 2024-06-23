92a3e9199e5d7eaaa07ffee7bc22230baf63ed75
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-06-23 20:32:34 +0700

    test-route

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,27 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about',['title' => 'About Page'],['name' => 'Rio Ragil Ramdani']);
 });
-Route::get('/blog', function () {
-    return view('blog',['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts',['title' => 'Blog Page', 'posts' => 
+    [
+        'title' => 'Judul Ark 1',
+        'author' => 'Rio Ragil Ramdani',
+        'body' => 'Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Quod, 
+        sapiente. Unde vero perferendis cumque architecto a 
+        adipisci laborum, dolor cupiditate dolorem, id accusantium. Quia officiis necessitatibus repellendus mollitia, numquam ratione? Quae.'
+    
+    ],
+    [
+        'title' => 'Judul Ark 2',
+        'author' => 'Rio Ragil Ramdani',
+        'body' => 'Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Quod, 
+        sapiente. Unde vero perferendis cumque architecto a 
+        adipisci laborum, dolor cupiditate dolorem, id accusantium. Quia officiis necessitatibus repellendus mollitia, numquam ratione? Quae.'
+    
+    ],
+]);
 });
 Route::get('/contact', function () {
     return view('contact',['title' => 'Contact Page']);
